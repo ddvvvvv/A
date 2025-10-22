ae5817f6de6b6e9b27370f7004630a502670162a
ddvvvvv <dvv324934@gmail.com>
2025-10-22 20:36:41 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: 'A',
+    resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 正确声明别名
+    },
+  },
 })
